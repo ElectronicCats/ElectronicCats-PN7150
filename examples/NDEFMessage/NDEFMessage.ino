23b71c3d76335a54caf5312c9c6f9990cdfa7c4d
deimos <deimoshall@gmail.com>
2023-07-17 13:07:30 -0600

    fix: config nfc mode with peer to peer fails

--- examples/NDEFMessage/NDEFMessage.ino
+++ examples/NDEFMessage/NDEFMessage.ino
@@ -14,14 +14,16 @@
 
 #include "Electroniccats_PN7150.h"
 #include "ndef_helper.h"
-#define PN7150_IRQ (15)
-#define PN7150_VEN (14)
+// #define PN7150_IRQ (15)  // Original
+#define PN7150_IRQ (11)  // BomberCat
+// #define PN7150_VEN (14)  // Original
+#define PN7150_VEN (13)  // BomberCat
 #define PN7150_ADDR (0x28)
 
 Electroniccats_PN7150 nfc(PN7150_IRQ, PN7150_VEN, PN7150_ADDR);  // Creates a global NFC device interface object, attached to pins 7 (IRQ) and 8 (VEN) and using the default I2C address 0x28
 RfIntf_t RfInterface;                                            // Interface to save data for multiple tags
 
-uint8_t mode = 3;  // modes: 1 = Reader/ Writer, 2 = Emulation, 3 = Peer to peer P2P
+uint8_t mode = 1;  // modes: 1 = Reader/ Writer, 2 = Emulation, 3 = Peer to peer P2P
 
 // #if defined P2P_SUPPORT || defined CARDEMU_SUPPORT
 const char NDEF_MESSAGE[] = {0xD1,      // MB/ME/CF/1/IL/TNF
@@ -32,7 +34,7 @@ const char NDEF_MESSAGE[] = {0xD1,      // MB/ME/CF/1/IL/TNF
                              'e', 'n',  // Language
                              'T', 'e', 's', 't'};
 
-void NdefPush_Cb(unsigned char *pNdefRecord, unsigned short NdefRecordSize) {
+void ndefPush_Cb(unsigned char *pNdefRecord, unsigned short NdefRecordSize) {
   Serial.println("--- NDEF Record sent");
 }
 // #endif // if defined P2P_SUPPORT || defined CARDEMU_SUPPORT
@@ -44,163 +46,168 @@ void setup() {
   Serial.println("NDEF Message with PN7150");
 
   /* Register NDEF message to be sent to remote peer */
-  // P2P_NDEF_SetMessage((unsigned char *) NDEF_MESSAGE, sizeof(NDEF_MESSAGE), *NdefPush_Cb);
+  // P2P_NDEF_SetMessage((unsigned char *) NDEF_MESSAGE, sizeof(NDEF_MESSAGE), *ndefPush_Cb);
   /* Register callback for reception of NDEF message from remote peer */
-  //   P2P_NDEF_RegisterPullCallback(*NdefPull_Cb);
-//   P2P_NDEF_RegisterPullCallback(reinterpret_cast<void *>(NdefPull_Cb));
+  //   P2P_NDEF_RegisterPullCallback(*ndefPull_Cb);
+  P2P_NDEF_RegisterPullCallback(reinterpret_cast<void *>(ndefPull_Cb));
 
-//   Serial.println("Initializing...");
-//   if (nfc.connectNCI()) {  // Wake up the board
-//     Serial.println("Error while setting up the mode, check connections!");
-//     while (1)
-//       ;
-//   }
+  Serial.println("Initializing...");
+  if (nfc.connectNCI()) {  // Wake up the board
+    Serial.println("Error while setting up the mode, check connections!");
+    while (1)
+      ;
+  }
 
-//   if (nfc.ConfigureSettings()) {
-//     Serial.println("The Configure Settings failed!");
-//     while (1)
-//       ;
-//   }
+  if (nfc.ConfigureSettings()) {
+    Serial.println("The Configure Settings failed!");
+    while (1)
+      ;
+  }
 
-//   if (nfc.ConfigMode(mode)) {  // Set up the configuration mode
-//     Serial.println("The Configure Mode failed!!");
-//     while (1)
-//       ;
-//   }
-//   nfc.StartDiscovery(mode);  // NCI Discovery mode
-//   Serial.println("Waiting for a NDEF device...");
+  // TODO: Initialize the NFC controller with mode = 2 or 3 will fail, check this later
+	// Temporal fix: Initialize the NFC controller with mode = 1 and then change to mode = 2 or 3 with the resetMode() function
+  if (nfc.ConfigMode(mode)) {  // Set up the configuration mode
+    Serial.println("The Configure Mode failed!!");
+    while (1)
+      ;
+  }
+  nfc.StartDiscovery(mode);  // NCI Discovery mode
+  Serial.println("Waiting for a NDEF device...");
+
+  mode = 3;
+  resetMode();
 }
 
 void loop() {
-//   if (!nfc.WaitForDiscoveryNotification(&RfInterface)) {  // Waiting to detect
-//     if (RfInterface.Interface == INTF_NFCDEP) {
-//       if ((RfInterface.ModeTech & MODE_LISTEN) == MODE_LISTEN)
-//         Serial.println(" - P2P TARGET MODE: Activated from remote Initiator");
-//       else
-//         Serial.println(" - P2P INITIATOR MODE: Remote Target activated");
+  if (!nfc.WaitForDiscoveryNotification(&RfInterface)) {  // Waiting to detect
+    if (RfInterface.Interface == INTF_NFCDEP) {
+      if ((RfInterface.ModeTech & MODE_LISTEN) == MODE_LISTEN)
+        Serial.println(" - P2P TARGET MODE: Activated from remote Initiator");
+      else
+        Serial.println(" - P2P INITIATOR MODE: Remote Target activated");
 
-//       /* Process with SNEP for NDEF exchange */
-//       nfc.ProcessP2pMode(RfInterface);
-//       Serial.println("Peer lost!");
-//     }
-//     ResetMode();
-//   }
+      /* Process with SNEP for NDEF exchange */
+      nfc.ProcessP2pMode(RfInterface);
+      Serial.println("Peer lost!");
+    }
+    resetMode();
+  }
   delay(500);
 }
 
-// void ResetMode() {  // Reset the configuration mode after each reading
-//   Serial.println("Re-initializing...");
-//   nfc.ConfigMode(mode);
-//   nfc.StartDiscovery(mode);
-// }
+void resetMode() {  // Reset the configuration mode after each reading
+  Serial.println("Re-initializing...");
+  nfc.ConfigMode(mode);
+  nfc.StartDiscovery(mode);
+}
 
-// void NdefPull_Cb(unsigned char *pNdefMessage, unsigned short NdefMessageSize) {
-//   unsigned char *pNdefRecord = pNdefMessage;
-//   NdefRecord_t NdefRecord;
-//   unsigned char save;
+void ndefPull_Cb(unsigned char *pNdefMessage, unsigned short NdefMessageSize) {
+  unsigned char *pNdefRecord = pNdefMessage;
+  NdefRecord_t NdefRecord;
+  unsigned char save;
 
-//   if (pNdefMessage == NULL) {
-//     Serial.println("--- Provisioned buffer size too small or NDEF message empty");
-//     return;
-//   }
+  if (pNdefMessage == NULL) {
+    Serial.println("--- Provisioned buffer size too small or NDEF message empty");
+    return;
+  }
 
-//   while (pNdefRecord != NULL) {
-//     Serial.println("--- NDEF record received:");
+  while (pNdefRecord != NULL) {
+    Serial.println("--- NDEF record received:");
 
-//     NdefRecord = DetectNdefRecordType(pNdefRecord);
+    NdefRecord = DetectNdefRecordType(pNdefRecord);
 
-//     switch (NdefRecord.recordType) {
-//       case MEDIA_VCARD: {
-//         save = NdefRecord.recordPayload[NdefRecord.recordPayloadSize];
-//         NdefRecord.recordPayload[NdefRecord.recordPayloadSize] = '\0';
-//         Serial.print("vCard:");
-//         // Serial.println(NdefRecord.recordPayload);
-//         NdefRecord.recordPayload[NdefRecord.recordPayloadSize] = save;
-//       } break;
+    switch (NdefRecord.recordType) {
+      case MEDIA_VCARD: {
+        save = NdefRecord.recordPayload[NdefRecord.recordPayloadSize];
+        NdefRecord.recordPayload[NdefRecord.recordPayloadSize] = '\0';
+        Serial.print("vCard:");
+        // Serial.println(NdefRecord.recordPayload);
+        NdefRecord.recordPayload[NdefRecord.recordPayloadSize] = save;
+      } break;
 
-//       case WELL_KNOWN_SIMPLE_TEXT: {
-//         save = NdefRecord.recordPayload[NdefRecord.recordPayloadSize];
-//         NdefRecord.recordPayload[NdefRecord.recordPayloadSize] = '\0';
-//         Serial.print("Text record:");
-//         // Serial.println(&NdefRecord.recordPayload[NdefRecord.recordPayload[0]+1]);
-//         NdefRecord.recordPayload[NdefRecord.recordPayloadSize] = save;
-//       } break;
+      case WELL_KNOWN_SIMPLE_TEXT: {
+        save = NdefRecord.recordPayload[NdefRecord.recordPayloadSize];
+        NdefRecord.recordPayload[NdefRecord.recordPayloadSize] = '\0';
+        Serial.print("Text record:");
+        // Serial.println(&NdefRecord.recordPayload[NdefRecord.recordPayload[0]+1]);
+        NdefRecord.recordPayload[NdefRecord.recordPayloadSize] = save;
+      } break;
 
-//       case WELL_KNOWN_SIMPLE_URI: {
-//         save = NdefRecord.recordPayload[NdefRecord.recordPayloadSize];
-//         NdefRecord.recordPayload[NdefRecord.recordPayloadSize] = '\0';
-//         Serial.print("URI record: ");
-//         // Serial.println(ndef_helper_UriHead(NdefRecord.recordPayload[0]), &NdefRecord.recordPayload[1]);
-//         NdefRecord.recordPayload[NdefRecord.recordPayloadSize] = save;
-//       } break;
+      case WELL_KNOWN_SIMPLE_URI: {
+        save = NdefRecord.recordPayload[NdefRecord.recordPayloadSize];
+        NdefRecord.recordPayload[NdefRecord.recordPayloadSize] = '\0';
+        Serial.print("URI record: ");
+        // Serial.println(ndef_helper_UriHead(NdefRecord.recordPayload[0]), &NdefRecord.recordPayload[1]);
+        NdefRecord.recordPayload[NdefRecord.recordPayloadSize] = save;
+      } break;
 
-//       case MEDIA_HANDOVER_WIFI: {
-//         unsigned char index = 0, i;
+      case MEDIA_HANDOVER_WIFI: {
+        unsigned char index = 0, i;
 
-//         Serial.println("--- Received WIFI credentials:");
-//         if ((NdefRecord.recordPayload[index] == 0x10) && (NdefRecord.recordPayload[index + 1] == 0x0e))
-//           index += 4;
-//         while (index < NdefRecord.recordPayloadSize) {
-//           if (NdefRecord.recordPayload[index] == 0x10) {
-//             if (NdefRecord.recordPayload[index + 1] == 0x45) {
-//               Serial.print("- SSID = ");
-//               for (i = 0; i < NdefRecord.recordPayload[index + 3]; i++)
-//                 Serial.print(NdefRecord.recordPayload[index + 4 + i]);
-//               Serial.println("");
-//             } else if (NdefRecord.recordPayload[index + 1] == 0x03) {
-//               Serial.print("- Authenticate Type = ");
-//               Serial.println(ndef_helper_WifiAuth(NdefRecord.recordPayload[index + 5]));
-//             } else if (NdefRecord.recordPayload[index + 1] == 0x0f) {
-//               Serial.print("- Encryption Type = ");
-//               Serial.println(ndef_helper_WifiEnc(NdefRecord.recordPayload[index + 5]));
-//             } else if (NdefRecord.recordPayload[index + 1] == 0x27) {
-//               Serial.print("- Network key = ");
-//               for (i = 0; i < NdefRecord.recordPayload[index + 3]; i++)
-//                 Serial.print("#");
-//               Serial.println("");
-//             }
-//             index += 4 + NdefRecord.recordPayload[index + 3];
-//           } else
-//             continue;
-//         }
-//       } break;
+        Serial.println("--- Received WIFI credentials:");
+        if ((NdefRecord.recordPayload[index] == 0x10) && (NdefRecord.recordPayload[index + 1] == 0x0e))
+          index += 4;
+        while (index < NdefRecord.recordPayloadSize) {
+          if (NdefRecord.recordPayload[index] == 0x10) {
+            if (NdefRecord.recordPayload[index + 1] == 0x45) {
+              Serial.print("- SSID = ");
+              for (i = 0; i < NdefRecord.recordPayload[index + 3]; i++)
+                Serial.print(NdefRecord.recordPayload[index + 4 + i]);
+              Serial.println("");
+            } else if (NdefRecord.recordPayload[index + 1] == 0x03) {
+              Serial.print("- Authenticate Type = ");
+              Serial.println(ndef_helper_WifiAuth(NdefRecord.recordPayload[index + 5]));
+            } else if (NdefRecord.recordPayload[index + 1] == 0x0f) {
+              Serial.print("- Encryption Type = ");
+              Serial.println(ndef_helper_WifiEnc(NdefRecord.recordPayload[index + 5]));
+            } else if (NdefRecord.recordPayload[index + 1] == 0x27) {
+              Serial.print("- Network key = ");
+              for (i = 0; i < NdefRecord.recordPayload[index + 3]; i++)
+                Serial.print("#");
+              Serial.println("");
+            }
+            index += 4 + NdefRecord.recordPayload[index + 3];
+          } else
+            continue;
+        }
+      } break;
 
-//       case WELL_KNOWN_HANDOVER_SELECT:
-//         Serial.print("Handover select version ");
-//         Serial.print(NdefRecord.recordPayload[0] >> 4);
-//         Serial.println(NdefRecord.recordPayload[0] & 0xF);
-//         break;
+      case WELL_KNOWN_HANDOVER_SELECT:
+        Serial.print("Handover select version ");
+        Serial.print(NdefRecord.recordPayload[0] >> 4);
+        Serial.println(NdefRecord.recordPayload[0] & 0xF);
+        break;
 
-//       case WELL_KNOWN_HANDOVER_REQUEST:
-//         Serial.print("Handover request version ");
-//         Serial.print(NdefRecord.recordPayload[0] >> 4);
-//         Serial.println(NdefRecord.recordPayload[0] & 0xF);
-//         break;
+      case WELL_KNOWN_HANDOVER_REQUEST:
+        Serial.print("Handover request version ");
+        Serial.print(NdefRecord.recordPayload[0] >> 4);
+        Serial.println(NdefRecord.recordPayload[0] & 0xF);
+        break;
 
-//       case MEDIA_HANDOVER_BT:
-//         Serial.print("BT Handover payload = ");
-//         // Serial.print(NdefRecord.recordPayload);
-//         // Serial.println(NdefRecord.recordPayloadSize);
-//         break;
+      case MEDIA_HANDOVER_BT:
+        Serial.print("BT Handover payload = ");
+        // Serial.print(NdefRecord.recordPayload);
+        // Serial.println(NdefRecord.recordPayloadSize);
+        break;
 
-//       case MEDIA_HANDOVER_BLE:
-//         Serial.print("BLE Handover payload = ");
-//         // Serial.print(NdefRecord.recordPayload);
-//         // Serial.println(NdefRecord.recordPayloadSize);
-//         break;
+      case MEDIA_HANDOVER_BLE:
+        Serial.print("BLE Handover payload = ");
+        // Serial.print(NdefRecord.recordPayload);
+        // Serial.println(NdefRecord.recordPayloadSize);
+        break;
 
-//       case MEDIA_HANDOVER_BLE_SECURE:
-//         Serial.print("   BLE secure Handover payload = ");
-//         // Serial.println(NdefRecord.recordPayload, NdefRecord.recordPayloadSize);
-//         break;
+      case MEDIA_HANDOVER_BLE_SECURE:
+        Serial.print("   BLE secure Handover payload = ");
+        // Serial.println(NdefRecord.recordPayload, NdefRecord.recordPayloadSize);
+        break;
 
-//       default:
-//         Serial.println("Unsupported NDEF record, cannot parse");
-//         break;
-//     }
-//     pNdefRecord = GetNextRecord(pNdefRecord);
-//   }
+      default:
+        Serial.println("Unsupported NDEF record, cannot parse");
+        break;
+    }
+    pNdefRecord = GetNextRecord(pNdefRecord);
+  }
 
-//   Serial.println("");
-// }
+  Serial.println("");
+}
 // #endif // if defined P2P_SUPPORT || defined RW_SUPPORT
